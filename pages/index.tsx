680cbc7b8ebe5639fcdf634cee2c21d248ca0566
EduardoWeschenfelder <eduardoweschenfelder@alunos.utfpr.edu.br>
2021-01-31 20:51:59 -0300

    Updates

---
+++ pages/index.tsx
@@ -0,0 +1,17 @@
+import React, { useEffect, useState } from "react";
+import Head from "next/head";
+
+import { WebcamCapture } from "./components/webcam";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Webcam</title>
+      </Head>
+      <div className="max-w-7xl max-h-screen mx-auto px-4 sm:px-2 ">
+        <WebcamCapture />
+      </div>
+    </>
+  );
+}
