fdfb74c3584f888e856fa4ba9b5b7a53b4e468e2
EduardoWeschenfelder <eduardoweschenfelder@alunos.utfpr.edu.br>
2021-02-01 15:24:17 -0300

    fix(next): urls

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@ const isProd = process.env.NODE_ENV === "production";
 
 module.exports = {
   ...(isProd
-    ? { basePath: "/webcamera.com.br", assetPrefix: "/webcamera.com.br" }
+    ? {
+        basePath: "/webcamera.com.br",
+        assetPrefix:
+          "https://cdn.statically.io/gh/etcdigital/webcamera.com.br/gh-pages/",
+      }
     : {}),
 };
