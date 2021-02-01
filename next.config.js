484af0337b19cb7121d30eda5b9e0401aca2364f
EduardoWeschenfelder <eduardoweschenfelder@alunos.utfpr.edu.br>
2021-02-01 14:20:35 -0300

    feet(next): exportPathMap

--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@ const isProd = process.env.NODE_ENV === "production";
 
 module.exports = {
   // Use the CDN in production and localhost for development.
+  exportPathMap: async function (
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    return {
+      "/": { page: "/" },
+    };
+  },
   assetPrefix: isProd
     ? "https://cdn.statically.io/gh/etcdigital/etcdigital.github.io/gh-pages/"
     : "",
