0159a17fe8ae77c61d170196dad64c9dc0a06600
EduardoWeschenfelder <eduardoweschenfelder@alunos.utfpr.edu.br>
2021-02-01 15:16:03 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,7 @@
 const isProd = process.env.NODE_ENV === "production";
 
 module.exports = {
-  // Use the CDN in production and localhost for development.
-  exportPathMap: async function (
-    defaultPathMap,
-    { dev, dir, outDir, distDir, buildId }
-  ) {
-    return {
-      "/": { page: "/" },
-    };
-  },
+  ...(isProd
+    ? { basePath: "/webcamera.com.br", assetPrefix: "/webcamera.com.br" }
+    : {}),
 };
